c437e49af1f7a65df851fb17f17e2195b146c3cc
Dariusz Okoński <darek200180@gmail.com>
2024-04-24 10:57:22 +0200

    add links

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const About = () => {
   return (
     <div>
       <h2>About</h2>
+      <Link to='/'>Home Page</Link>
     </div>
   );
 };
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomeLayout = () => {
   return (
     <div>
       <h2>HomeLayout</h2>
+      <Link to='/about'>About Page</Link>
     </div>
   );
 };
