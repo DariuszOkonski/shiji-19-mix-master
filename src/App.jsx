23247e0cd06660f71a2dc2fb156ea88e18749f0a
Dariusz Okoński <darek200180@gmail.com>
2024-04-24 10:42:38 +0200

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
+import {
+  BrowserRouter,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  { path: '/', element: <h1>home page</h1> },
+  {
+    path: '/about',
+    element: (
+      <div>
+        <h2>about page</h2>
+      </div>
+    ),
+  },
+]);
+
 const App = () => {
-  return <h2>mixmaster starter</h2>;
+  return <RouterProvider router={router} />;
 };
 export default App;
