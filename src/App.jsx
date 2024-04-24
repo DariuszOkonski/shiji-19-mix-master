91ea727252f6fdba8843ed23a0dff36375536d7f
Dariusz Okoński <darek200180@gmail.com>
2024-04-24 11:16:31 +0200

    add nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,27 @@ import {
 } from './pages';
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomeLayout /> },
   {
-    path: '/about',
-    element: <About />,
+    path: '/',
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'cocktail',
+        element: <Cocktail />,
+      },
+      {
+        path: 'newsletter',
+        element: <Newsletter />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+    ],
   },
 ]);
 
