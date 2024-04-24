3d291169fecc026b3e8bc57beb8a0c7ef52e822b
Dariusz Okoński <darek200180@gmail.com>
2024-04-24 11:58:38 +0200

    style AboutPage

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/AboutPage';
 
 const About = () => {
   return (
-    <div>
-      <h2>About</h2>
-      <Link to='/'>Home Page</Link>
-    </div>
+    <Wrapper>
+      <h3>About Us</h3>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis
+        repudiandae provident nostrum assumenda blanditiis totam, accusamus
+        explicabo tempore laudantium pariatur labore nemo aliquid neque
+        perspiciatis, sed optio quasi nisi odio!. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Cum quo assumenda facere rerum fugiat
+        maxime a dolor vel illum, recusandae culpa nam magni inventore magnam
+        obcaecati explicabo pariatur? Veritatis, eius!
+      </p>
+    </Wrapper>
   );
 };
 
