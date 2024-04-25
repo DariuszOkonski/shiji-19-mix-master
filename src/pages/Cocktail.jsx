b7928aaf9a04fc580297095604873d2eb14041d0
Dariusz Okoński <darek200180@gmail.com>
2024-04-25 12:38:56 +0200

    add loader for Cocktail

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { useLoaderData, Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/CocktailPage';
+import axios from 'axios';
+
+const singleCocktailUrl =
+  'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+
+export const loader = async ({ params }) => {
+  const { id } = params;
+
+  const { data } = await axios.get(`${singleCocktailUrl}${id}`);
+  return { data, id };
+};
 
 const Cocktail = () => {
+  const { data, id } = useLoaderData();
+
   return (
-    <div>
+    <Wrapper>
       <h2>Cocktail</h2>
-    </div>
+    </Wrapper>
   );
 };
 
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,12 +1,21 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 
 const HomeLayout = () => {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === 'loading';
+
   return (
     <>
       <Navbar />
       <section className='page'>
-        <Outlet />
+        {isPageLoading ? (
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <div className='loading' />
+          </div>
+        ) : (
+          <Outlet />
+        )}
       </section>
     </>
   );
